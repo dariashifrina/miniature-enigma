249b764c5ef3bc9b563b844304ebdecc4fa9b3b4
queeniexiang <wxiang1@stuy.edu>
2017-11-16 21:06:17 -0500

    Updated without random time for each child process

--- forkin.c
+++ forkin.c
@@ -1,3 +1,10 @@
+/* 
+Queenie Xiang and Dasha Shifrina 
+Systems pd5
+HW12: Please Fork Responsibly
+2017-11-16
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,21 +12,51 @@
 #include <sys/types.h>
 
 int main(){
-  srand(time(NULL));
-  int f, t;
-  printf("Zoo wee mama!\n");
+  //Creating variables 
+  int f1, f2, t, signal;
 
-  fork();
-  //  f = fork(); //fork() returns the PID of the child process
-  t = rand() % 15 + 5;
-  if (f == 0) {
-    sleep(t); //child is asleep for t seconds!
-    printf("I'm a child: %d\t f:%d\tparent: %d\t waited for: %d seconds\n", getpid(), f, getppid(), t);
-    printf("I, the child, am finished.\n");
-  } else {
-    printf("waiting for lazy child\n");
-    wait(&t); //wait for child to sleep for 2 seconds!
-    printf("I'm a parent: %d\t f:%d\tparent: %d\n", getpid(), f, getppid());
+  //Seeding rand() with time 
+  srand(time(NULL));
+
+  
+  printf("Zoo wee mama! Starting forking! \n");
+  printf("*****************************\n");
+  printf("I'm parent: %d\t f:%d\t\n", getpid(), f1);
+
+
+  f1 = fork(); //Creating first fork 
+  
+  if (f1 == 0) {
+    t = rand() % 15 + 5;
+    //signal = t; 
+    sleep(t);
+    printf("Hi, I'm child 1: %d\t f:%d\t Parent: %d\t Slept for: %d seconds\n", getpid(), f1, getppid(), t);
+    printf("I, child 1, am finished.\n");
+    printf("*****************************\n");
+    return t; 
   }
+  
+  else {
+    printf("*****************************\n");
+    f2 = fork();
+    if (f2 == 0) {
+      t = rand() % 15 + 5;
+      sleep(t); 
+      printf("Hi, I'm child 2: %d\t f:%d\t Parent: %d\t Slept for: %d seconds\n", getpid(), f2, getppid(), t);
+      printf("I, child 2, am finished.\n");
+      printf("*****************************\n");
+      signal = t;
+      return t; 
+    }
+
+    else {
+      printf("Waiting for lazy child...\n");
+      printf("*****************************\n");
+      wait(&signal);
+      //WEXITSTATUS will return whatever the child returned before exiting 
+      printf("Child slept for: %d\n seconds", WEXITSTATUS(signal)); 
+    }
+  }
+  
   return 0;
 }
